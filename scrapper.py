319ee6235068335e03c6ed30e1a4b051b7d5c4cf
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-19 10:57:24 -0300

    click no botao de login

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 url = "https://nfps-e.pmf.sc.gov.br/frontend/#!/login"
 
 driver = webdriver.Chrome()
 
 driver.get(url)
+login_buttons = WebDriverWait(driver, timeout=10).until(
+    lambda driver: driver.find_elements(By.CLASS_NAME, "label-recarrega")
+)
+
+login_button_list = list(
+    filter(
+        lambda elem: "Realizar login sem certificado digital" in elem.text,
+        login_buttons
+    )
+)
+
+login_button = None
+if login_button_list:
+    login_button = login_button_list[0]
+
+if not login_button:
+    raise Exception("Login button not founded")
+
+login_button.click()
+
