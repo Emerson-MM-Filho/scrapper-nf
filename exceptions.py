40da5f9e5742a53c974e78c3b4b243eab4be5e67
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-22 22:26:03 -0300

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,16 @@
+class ElementNotFounded(Exception):
+    def __init__(self, element_name: str):
+        self.element_name: str = element_name
+
+    @property
+    def error_message(self) -> str:
+        return f"Element not founded: {self.element_name.title()}"
+
+class FiscalNoteException(Exception):
+    def __init__(self, action: str, error: str):
+        self.action: str = action
+        self.error: str = error
+
+    @property
+    def error_message(self) -> str:
+        return f"{self.action} - {self.error_message}"
